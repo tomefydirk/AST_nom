249ece048c36b82bd2390d3a13288680835b6902
tomefydirk <tomefydirk@yahoo.com>
2025-07-18 22:11:10 +0300

    delete unused function

--- src/main.rs
+++ src/main.rs
@@ -63,36 +63,6 @@ fn parser_number(input: &str) -> f64{
     input.parse().expect("Parsing en f64 (erreur) ")
 }
 
-/*fn get_number(input: &str) -> Option<(f64 , usize)> {
-    let mut ex_number=String::new();
-    let mut index=0;
-    for (current_index,a ) in input.chars().enumerate(){
-        println!("{a}");
-        if a=='-' || a=='.' || a.is_ascii_digit() {
-
-            ex_number=format!("{ex_number}{a}");
-
-
-        }else if ex_number.parse::<f64>().is_ok(){
-
-          return  Some(( parser_number(&ex_number)   , current_index ) );
-      
-        }else {
-
-           return None;
-
-        }
-        index=current_index;
-    }
-
-    if ex_number.parse::<f64>().is_ok() {
-         return  Some(( parser_number(&ex_number)   , index ) );
-
-    }else{
-        return None;
-    } 
-}*/
-
 fn premier_nombre(phrase: &str) -> Option<f64> {
     let re = Regex::new(r"-?\d+(\.\d+)?").unwrap();
     if let Some(mat) = re.find(phrase) {
