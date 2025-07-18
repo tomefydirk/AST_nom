6ec426706860d1838465bb35471b6d65afadd6a4
tomefydirk <tomefydirk@yahoo.com>
2025-07-18 21:04:00 +0300

    function eval done

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use nom::{
-    IResult,
-    character::complete::digit1,
-    combinator::map_res,
+    character::complete::digit1, combinator::map_res, number, IResult
 };
 #[derive(Debug)]
 pub enum Expr {
-    Number(i64),
+    Number(Number)
     BinaryOp {
         left: Box<Expr>,
         op: BinOp,
@@ -20,7 +18,42 @@ pub enum BinOp {
     Mul,
     Div,
 }
+pub enum Number{
+    Pos(f64),
+    Neg(f64)
+}
+pub  trait Evaluable {
+    fn eval(&self) -> f64 ;   
+}
+impl  Evaluable for Number {
+    fn eval(&self) -> f64  {
+        match self {
+            Number::Pos(n) => *n ,
+            Number::Neg(n) => -(*n),
+        }
+    }
+}
+impl Evaluable for Expr {
+    fn eval(&self) -> f64 {
+        match self {
+            Expr::Number(n) => n.eval(),
+            Expr::BinaryOp { left, op, right } => {
+                let l = left.eval();
+                let r = right.eval();
+                match op {
+                    BinOp::Add => l + r,
+                    BinOp::Sub => l - r,
+                    BinOp::Mul => l * r,
+                    BinOp::Div => l / r,
+                }
+            }
+        }
+    }
+}
 
+fn parser(expr_str:&String) -> Expr {
+    todo!()
+}
 fn main(){
 
 }
