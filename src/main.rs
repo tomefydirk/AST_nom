131df1bd57f3432fc58a519f4028508415ca5e53
tomefydirk <tomefydirk@yahoo.com>
2025-07-21 15:16:44 +0300

    testing scantoken

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 
-use std::str::FromStr;
+use std::{char, result, str::FromStr};
 
 use nom::{
-    character::complete::{digit1, space0}, combinator::map_res, number, sequence::delimited, IResult
+    branch::alt, bytes::complete::tag, character::complete::digit1, combinator::opt, sequence::pair, IResult, Parser
 };
-use regex::Regex;
-
 
 #[derive(Debug)]
 pub enum Expr {
-    Number(Number),
+    Number(i32),
     BinaryOp {
         left: Box<Expr>,
         op: BinOp,
@@ -24,55 +22,34 @@ pub enum BinOp {
     Mul,
     Div,
 }
-
-#[derive(Debug)]
-pub enum Number{
-    Pos(f64),
-    Neg(f64)
+fn scan_plus(input:&str)-> IResult<&str,&str>{
+    tag("+")(input)
+}
+fn scan_moins(input:&str)-> IResult<&str,&str>{
+    tag("-")(input)
+}
+fn scan_digit(input:&str)-> IResult<&str,&str>{
+    digit1(input)
 }
 
-pub  trait Evaluable {
-    fn eval(&self) -> f64 ;   
+fn scantoken(input:&str) -> IResult<&str,&str>{
+
+    alt((
+        scan_digit,
+        scan_plus,
+        scan_moins
+    )).parse(input)
+
+    
 }
-impl  Evaluable for Number {
-    fn eval(&self) -> f64  {
-        match self {
-            Number::Pos(n) => *n ,
-            Number::Neg(n) => -(*n),
-        }
-    }
-}
-impl Evaluable for Expr {
-    fn eval(&self) -> f64 {
-        match self {
-            Expr::Number(n) => n.eval(),
-            Expr::BinaryOp { left, op, right } => {
-                let l = left.eval();
-                let r = right.eval();
-                match op {
-                    BinOp::Add => l + r,
-                    BinOp::Sub => l - r,
-                    BinOp::Mul => l * r,
-                    BinOp::Div => l / r,
-                }
-            }
-        }
-    }
+fn parse_expr(input:&str)->Expr{
+    todo!()
 }
 
-fn premier_nombre(phrase: &str) -> Option<f64> {
-    let re = Regex::new(r"-?\d+(\.\d+)?").unwrap();
-    if let Some(mat) = re.find(phrase) {
-        mat.as_str().parse::<f64>().ok()
-    } else {
-        None
-    }
+fn main(){
+    let a="312,2";
+    let v=scantoken(a);
+    println!("{v:?}");
 }
 
-fn main() {
-    let texte = "La température est descendue à -1212.5 degrés hier soir.";
-    match premier_nombre(texte) {
-        Some(n) => println!("Premier nombre trouvé : {}", n),
-        None => println!("Aucun nombre trouvé."),
-    }
-}
+
