d5ea2cf9cfe51972acc413c83afaf8a893869b0a
tomefydirk <tomefydirk@yahoo.com>
2025-07-21 18:52:12 +0300

    parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 
 use std::{char, result, str::FromStr};
 
+use nom::sequence::delimited;
+use nom::Input;
 use nom::{
-    branch::alt, bytes::complete::tag, character::complete::digit1, combinator::opt, sequence::pair, IResult, Parser
+    bytes::complete::{tag, take_while1},
+    character::complete::char,
+    branch::alt, character::complete::digit1, combinator::opt, sequence::pair, Err, IResult, Parser
 };
+use nom::error::{Error, ParseError};
 
 #[derive(Debug)]
 pub enum Expr {
@@ -23,6 +28,24 @@ pub enum BinOp {
     Mul,
     Div,
 }
+impl BinOp {
+    fn from_str(a:&str)->Self{
+        match a {
+            "+"=>BinOp::Add,
+            "-"=>BinOp::Sub,
+            "*"=>BinOp::Mul,
+            "/"=>BinOp::Div, 
+           _=>BinOp::Add
+        }
+    }
+}
+
+impl Expr{
+    fn result_binop_from(input:&str,left:Expr , right:Expr,operation:BinOp)->IResult<&str,Expr>{
+       let result=(input,Expr::BinaryOp { left: (Box::new(left)), op : operation, right: Box::new(right) });
+       IResult::Ok(result)
+    }
+}
 fn scan_plus(input:&str)-> IResult<&str,&str>{
     tag("+")(input)
 }
@@ -32,6 +55,26 @@ fn scan_moins(input:&str)-> IResult<&str,&str>{
 fn scan_digit(input:&str)-> IResult<&str,&str>{
     digit1(input)
 }
+fn parse_without_parens(input: &str) -> IResult<&str, &str> {
+   let (input_1,_)=tag("(").parse(input)?; 
+   let (input_farany,out_put)=take_while1(|c: char| c != ')').parse(input_1)?;
+   let (f,_)=tag(")").parse(input_farany)?;
+   IResult::Ok((f,out_put))
+}
+
+/*fn enlever_dernier_motif(input: &str, motif: &str) -> IResult<&str, &str> {
+    if motif.is_empty() {
+        return Ok((input, input)); // Rien à enlever
+    }
+
+    if let Some(pos) = input.rfind(motif) {
+        let (avant, _) = input.split_at(pos);
+        Ok(("", avant))
+    } else {
+        Ok(("", input)) // motif non trouvé → on renvoie l'input entier
+    }
+}*/
+
 
 fn scantoken(input:&str) -> IResult<&str,&str>{
 
@@ -44,39 +87,60 @@ fn scantoken(input:&str) -> IResult<&str,&str>{
     
 }
 fn parse_expr(input:&str)->IResult<&str,Expr>{
-    todo!()
+    let (reste,next_token)=scantoken(input)?;
+    let a=parse_expr(input);
+    
+    if next_token=="-" || next_token=="+" || next_token=="*" || next_token=="/" {
+            let (new_scaned,_)=scantoken(input)?; 
+            let b=parse_term(new_scaned);
+            return Expr::result_binop_from(new_scaned, a?.1,b?.1, BinOp::from_str(next_token));
+    }
+    Err(nom::Err::Error(Error::new(input, nom::error::ErrorKind::Digit)))
+
+    
 }
 fn parse_factor(input:&str)->IResult<&str,Expr>{
     let (reste,next_token)=scantoken(input)?;
     let expr:IResult<&str,Expr>= match next_token.trim() {
         "("=>{
-            let new_reste=scantoken(reste);
-            let new_expr=parse_expr(new_reste?.0);
-            match new_expr {
-                Ok(_) => todo!(),
-                Err(_) => todo!(),
-            }
-            todo!()
+            let (new_reste,token)=scantoken(reste)?;
+            let new_result=parse_expr(new_reste);
+        /*    match new_result {
+                Ok((current_expr,expr_value)) => {
+                    if token == ")"{
+                        return parse_expr(current_expr);
+                    }else {
+                        return   Err(nom::Err::Error(Error::new(input, nom::error::ErrorKind::Digit)))
+                    }
+
+                },
+                Err(err) => {
+                    return  Err(err);
+                },
+            } */
+           
+           todo!()
+           
         },
         "-"=>{
             let (new_reste,_)=scantoken(reste)?;
             return  parse_factor(new_reste);
-        }
+        },
         t => {
             let a=u32::from_str(t).map_err(|_|{
-                nom::Err::Error(nom::error::Error::new(t, nom::error::ErrorKind::Digit))
+                nom::Err::Error(Error::new(t, nom::error::ErrorKind::Digit))
             })?;
             return IResult::Ok((t,Expr::Number(a)));
         } 
     };
-    todo!()
-}
-fn parse_term(){
 
 }
+fn parse_term(input:&str)->IResult<&str,Expr>{
+    todo!()
+}
 fn main(){
-    let a="312,2";
-    let v=scantoken(a);
+    let a="(11() )";
+    let v=parse_without_parens(a);
     println!("{v:?}");
 }
 
